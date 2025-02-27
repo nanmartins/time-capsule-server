49758cd64665e1622390281b97e195651f50e222
Renan Martins <renanmartins989@gmail.com>
2025-02-27 17:09:00 +0000

    ...

--- index.js
+++ index.js
@@ -8,29 +8,16 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-// app.use(cors());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // Permite qualquer origem
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-
-  if (req.method === "OPTIONS") {
-    return res.status(200).json({});
-  }
-
-  next();
-});
-
+app.use(cors());
 app.use(bodyParser.json());
-mongoose.connect(process.env.MONGO_URI, {
-  serverSelectionTimeoutMS: 5000,
-});
 
 // Routes
 // const capsuleRoutes = require('./routes/capsuleRoutes');
 const authRoutes = require('./routes/authRoute');
 // app.use('/api/capsules', capsuleRoutes);
 app.use('/api/auth', authRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
